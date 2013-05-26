48d999fac71f866548104c20f8b1bb299e50df43
Shunfan Du <i@perry.asia>
2013-05-26 16:29:29 +0800

    more coverage

--- porter.py
+++ porter.py
@@ -9,11 +9,15 @@ import os
 import shutil
 
 
-__all__ = ['mkdir', 'copy', 'copy_to', 'move', 'move_to', 'FileExistsError']
+__all__ = ['mkdir', 'copy', 'copy_to', 'move', 'move_to', 'FileExists', 'FileNotFound']
 __version__ = '0.0.1'
 
 
-class FileExistsError(EnvironmentError):
+class FileExists(EnvironmentError):
+    pass
+
+
+class FileNotFound(EnvironmentError):
     pass
 
 
@@ -27,7 +31,7 @@ def mkdir(directory, ignore=False, force=False):
             try:
                 os.mkdir(directory)
             except OSError:
-                raise FileExistsError("%s is exist." % directory)
+                raise FileExists("'%s' is exist." % directory)
         elif force:
             shutil.rmtree(directory)
             os.mkdir(directory)
@@ -42,10 +46,13 @@ def copy(src, dst, ignore=False, force=False):
         - copy a file to a future destination.
         - copy a directory to a future destination.
     """
+    if not os.path.exists(src) and not ignore:
+        raise FileNotFound("'%s' is not found." % src)
+
     if os.path.isfile(src):
         if os.path.exists(dst):
             if not ignore and not force:
-                raise FileExistsError("%s is exist." % dst)
+                raise FileExists("'%s' is exist." % dst)
             elif force:
                 shutil.copyfile(src, dst)
         else:
@@ -56,7 +63,7 @@ def copy(src, dst, ignore=False, force=False):
                 try:
                     shutil.copytree(src, dst)
                 except OSError:
-                    raise FileExistsError("%s is exist." % dst)
+                    raise FileExists("'%s' is exist." % dst)
             elif force:
                 shutil.rmtree(dst)
                 shutil.copytree(src, dst)
--- test.py
+++ test.py
@@ -7,8 +7,12 @@ import porter
 test_porter = 'test_porter'
 dir1 = os.path.join(test_porter, 'dir1')
 dir2 = os.path.join(test_porter, 'dir2')
+dir3 = os.path.join(test_porter, 'dir3')
+dir3_dir4 = os.path.join(test_porter, 'dir3', 'dir4')
+dir4 = os.path.join(test_porter, 'dir4')
 f1 = os.path.join(test_porter, 'dir1', 'f1.txt')
 f2 = os.path.join(test_porter, 'dir2', 'f2.txt')
+f3 = os.path.join(test_porter, 'dir3', 'f3.txt')
 
 def create(file_path):
     with open(file_path, 'w') as f:
@@ -35,8 +39,10 @@ def test_copy_file():
 
 
 def test_copy_directory():
-    porter.copy(dir2, dir1, ignore=True)
-    porter.copy(dir2, dir1, force=True)
+    porter.copy(dir2, dir3)
+    porter.copy(dir2, dir3, ignore=True)
+    porter.copy(dir2, dir3, force=True)
+    porter.copy_to(dir2, dir1)
     porter.copy_to(dir2, dir1, ignore=True)
     porter.copy_to(dir2, dir1, force=True)
     assert os.path.exists(os.path.join(dir1, 'dir2')) == True
@@ -44,18 +50,27 @@ def test_copy_directory():
 
 
 def test_move_file():
-    porter.move(f2, f1, ignore=True)
-    porter.move(f2, f1, force=True)
+    porter.move(f2, f3)
+    porter.move(f2, f3, ignore=True)
+    create(f2)
+    porter.move(f2, f3, force=True)
+    create(f2)
+    porter.move_to(f2, dir1)
     porter.move_to(f2, dir1, ignore=True)
+    create(f2)
     porter.move_to(f2, dir1, force=True)
     assert os.path.exists(f1) == True
     assert os.path.exists(f2) == False
 
 
 def test_move_directory():
-    porter.move(os.path.join(dir1, 'dir2'), dir2, ignore=True)
-    porter.move(os.path.join(dir1, 'dir2'), dir2, force=True)
-    porter.move_to(os.path.join(dir1, 'dir2'), test_porter, ignore=True)
-    porter.move_to(os.path.join(dir1, 'dir2'), test_porter, force=True)
-    assert os.path.exists(os.path.join(dir1, 'dir2')) == False
-    assert os.path.exists(os.path.join(dir2)) == True
+    porter.move(dir2, dir3_dir4)
+    porter.move(dir2, dir3_dir4, ignore=True)
+    porter.mkdir(dir2)
+    porter.move(dir2, dir3_dir4, force=True)
+    porter.move_to(dir3_dir4, test_porter)
+    porter.move_to(dir3_dir4, test_porter, ignore=True)
+    porter.mkdir(dir3_dir4)
+    porter.move_to(dir3_dir4, test_porter, force=True)
+    assert os.path.exists(dir3_dir4) == False
+    assert os.path.exists(dir4) == True
